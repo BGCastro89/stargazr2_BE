c0440ced6212ee978347618bf65bd3503239fc6f
Brian G Castro <prismaticq@gmail.com>
2021-09-14 15:12:17 -0700

    try set port explicitly

--- main.py
+++ main.py
@@ -352,4 +352,4 @@ def get_stargaze_report():
     return response
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=False, host="0.0.0.0", port=8080)
